ff747dc8ffe3c74dc15e5dfe07a38ec9f57d7d37
gr5175777-del <gr5175777@gmail.com>
2026-03-30 21:54:42 +0530

    Add files via upload

---
+++ day1.int.py
@@ -0,0 +1,36 @@
+'''x = 5
+print(x)
+print("hello world")
+print("this a python program")
+
+z = "4+5"
+print(z)
+
+
+name = "raj"
+print(name)
+
+type = "wggg"
+print(type)'''
+
+'''
+##data types##
+
+x = 10.55        ##float
+print(type(x))
+y = 12           ##int
+print(type(y))
+
+z= 3+6j          ##complex
+print(type(z))'''
+
+a = 'hello'   
+b = "world"
+print(a)
+print(b)
+c ="today class is important"    ##str
+print(c)
+print(a+b)
+p = 6
+q = 10
+print(p+q)  ##sum of numbers
