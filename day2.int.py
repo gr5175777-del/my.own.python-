7a0ab091de2735d91da3adf45fdcd7b92f141e51
gr5175777-del <gr5175777@gmail.com>
2026-03-30 22:53:04 +0530

    Add files via upload

---
+++ day2.int.py
@@ -0,0 +1,29 @@
+#output
+
+'''print("raj")
+x = input("enter your name:")
+y = input("enter your name:")
+print(x+y)'''
+
+#input()method
+
+'''name = input("enter your name:")
+print(name)'''
+
+'''x = 65
+y = 5
+print(x+y)'''
+
+'''x = int(input("enter your number:"))
+y =int(input("enter your number:"))
+print(x+y)'''
+
+'''x = float(input("enter your number:"))
+y =float(input("enter your number:"))
+print(x+y)'''
+
+
+
+x = int(input("enter your name:"))
+y = float(input("enter your name:"))
+print(x+y)
