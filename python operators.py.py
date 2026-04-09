1479f1c0e7e016b9b2fad0113054de84ab3f47b1
gr5175777-del <gr5175777@gmail.com>
2026-04-09 23:37:37 +0530

    Add files via upload

---
+++ python operators.py.py
@@ -0,0 +1,63 @@
+#arthmetic operators
+#arthmetic operators are used for basic mathematical calculations.
+
+'''a = 10
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)'''
+
+#comparison operators
+#comparison operators compere two values and return True or Flase
+
+'''x = 10
+y = 5
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=10)
+print(x<=5)'''
+
+#assignment operators
+#assignment operators are used to assign oe update the value of a variable
+'''
+x = 10
+x += 5 
+x *= 2
+x -= 10
+
+print(x)'''
+
+#logical operators
+#logical operators are used to combine multiple conditions.they work similarly to AND,OR,and NOT gates in digital electronics
+'''
+x = 10
+y = 20
+print(x > 5 and y < 30)
+print(x >15 or y <30)
+print(not(x > 5))'''
+
+#membership operators
+#membership operators check if a value exist with in a sequence(like a list,string,or tuple).
+'''
+fruits = ["apple","banana","cherry"]
+
+print("apple" in fruits)
+print("graps" not in fruits)'''
+
+#identity operators
+#identity operators check whether two variables referr to the same object in memory.
+
+a = [1,2,3]
+b = a
+c = [1,2,3]
+
+print(a is b)
+print(a is c)
+print(a is not c)
