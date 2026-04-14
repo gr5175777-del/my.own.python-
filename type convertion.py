e4c0b788fd0823c6953664c165655095de9272d8
gr5175777-del <gr5175777@gmail.com>
2026-04-14 20:46:38 +0530

    Add files via upload

---
+++ type convertion.py
@@ -0,0 +1,27 @@
+#topic type conversions
+#slicing
+'''message = "hi ravi"
+part = message[3:7]
+print(part)''' 
+
+'''message = "hi ravi"
+part = message[3:]
+print(part)'''
+
+'''message = "hi ravi"
+part = message[:]
+print(part)'''
+
+'''message = "hi ravi"
+start_index = 0
+end_index = 7
+part = message[start_index:end_index]
+print(part)'''
+
+#type convertion
+
+a = "5"
+print(type(a))
+a= int(a)
+print(type(a))
+print(a)
