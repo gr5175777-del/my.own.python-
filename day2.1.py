5c18040df8ca5800cb794084fe8b6ad88f5de9a6
gr5175777-del <gr5175777@gmail.com>
2026-04-27 12:49:57 +0530

    Add files via upload

---
+++ day2.1.py
@@ -0,0 +1,38 @@
+'''a = int(input("enter your number:"))
+b = int(input("enter your number:"))
+
+print("sum:",a+b)
+print("sub:",a-b)
+print("mult:",a*b)
+print("percentage:",a/b)
+print("power:",b**3)
+print("power:",a**b)
+print("division:",a//b)'''
+
+#comparison operators <,>,<=,>=,!=
+'''comparaison operators are used to comparing between the operants what ever it me be operants number of operants
+we just used to that to compaer between two operants'''
+'''
+a = 20
+b = 12
+print(a>b)
+print(a<b)
+print(a==b)
+print(a>=b)
+print(b>=a)
+print(a!=b)'''
+
+#30 pass marks >>15
+'''marks = 15
+print("pass" if marks >= 30 else "fail")'''
+'''
+marks =15
+print("pass")
+marks>= 15
+marks<= 15
+print("fail")'''
+
+# logical operators: and ,or, not
+
+a = 10
+print(a>5 and a<20)
