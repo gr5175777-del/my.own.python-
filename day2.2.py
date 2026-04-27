97d4a070b5c022b5a6c8480720c56bdd2626bd73
gr5175777-del <gr5175777@gmail.com>
2026-04-27 12:50:27 +0530

    Add files via upload

---
+++ day2.2.py
@@ -0,0 +1,24 @@
+#logical operators: and, or, not
+'''
+x = 10
+y = 15
+
+print(x>9 and x!=5)'''
+
+'''
+a = 20
+b = 5
+
+print(a<30 and b<10)
+print(a>15 and b>3)
+print(a>15 or b>5)
+print(not a>12)
+print(not b>10)'''
+
+#membership operators: in, not in
+'''
+marks = [12,15,16,17,18]
+print(12 in marks)
+print(12 not in marks)
+print(40 not in marks)
+print(16 in marks)'''
