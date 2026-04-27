3f0f65414f95056429fba276fb1d27df14411bbf
gr5175777-del <gr5175777@gmail.com>
2026-04-27 12:50:56 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,28 @@
+#identity operators: is, is not
+'''a = 12
+b = 18
+print(a is b)
+print(a is not b)'''
+
+#assignment operators:=, +=, /=, %=, //=, **=, &=,!=,^=, >>=, <<=
+'''
+z = 45
+z+=5
+print(z)
+z-=5
+print(z)
+z*=3
+print(z)
+z/=2 
+print(z)
+z//=2
+print(z)'''
+ 
+
+#bitwisen operators: &, !, ~,^,>>,<<
+a = 5
+b = 3
+print(a&b)
+print(a^b)
+print(~b)
+ 
